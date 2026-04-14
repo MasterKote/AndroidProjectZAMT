33da0059aedfb0918b2361df1f226d78ea856eda
Artem <kanalfoksi1@gmail.com>
2026-04-14 17:24:49 +0300

    pr5littlechanges

--- app/src/main/java/com/example/androidprojectzamt/Screen/SignUp.kt
+++ app/src/main/java/com/example/androidprojectzamt/Screen/SignUp.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -31,135 +32,119 @@ import com.example.androidprojectzamt.ui.theme.Gray
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun SignUp(modifier: Modifier = Modifier) {
-    Box(modifier = modifier
+    Column( modifier = Modifier
         .fillMaxSize()
-        .background(
-            color = Color.White
-        ),
-        contentAlignment = Alignment.TopCenter
+        .background(color = Color.White)
+        .padding(start = 20.dp, end = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
+        Spacer(
+            modifier.height(103.dp)
+        )
+        Text(
+            text = "Добро пожаловать!",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(
+            modifier.height(23.dp)
+        )
+
+        Text(
+            text = "Войдите, чтобы пользоваться функциями приложения",
+            fontSize = 17.sp
+        )
+
+        Spacer(
+            modifier.height(68.dp)
+        )
+
+        Text(
             modifier = modifier
-                .width(335.dp)
-                .padding(start = 20.dp, end = 20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .height(14.dp)
+                .fillMaxWidth(),
+            text = "Вход по E-mail",
+            color = Gray
+        )
+
+        Spacer(
+            modifier.height(4.dp)
+        )
+
+        var buttonstate by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = buttonstate,
+            onValueChange = { buttonstate = it },
+            placeholder = { Text("example@mail.ru") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(54.dp),
+            shape = RoundedCornerShape(12.dp),
+            enabled = true,
+        )
+
+        Spacer(
+            modifier.height(32.dp)
+        )
+
+        Button(
+            onClick = { },
+            enabled = buttonstate.isNotEmpty(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = ButActive,
+                contentColor = Color.White,
+                disabledContainerColor = ButInactive,
+                disabledContentColor = Color.White
+            )
         ) {
-            Spacer(
-                modifier.height(61.dp)
-            )
             Text(
-                text = "Добро пожаловать!",
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold
-            )
-
-            Spacer(
-                modifier.height(23.dp)
-            )
-
-            Text(
-                text = "Войдите, чтобы пользоваться функциями приложения",
-                fontSize = 15.sp,
+                text = "Далее",
+                fontSize = 16.sp,
             )
         }
 
-        Column(
-            modifier = modifier
-                .width(335.dp),
-            horizontalAlignment = Alignment.Start
+
+        Spacer(
+            modifier.height(238.dp)
+        )
+
+        Text(
+            text = "Или войдите с помощью",
+            fontSize = 15.sp,
+            color = Gray
+        )
+
+        Spacer(
+            modifier.height(16.dp)
+        )
+
+        OutlinedButton(
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.outlinedButtonColors(
+                containerColor = Color.White,
+                contentColor = ButActive
+            ),
+            border = BorderStroke(1.5.dp, ButActive)
         ) {
-            Spacer(
-                modifier.height(262.dp)
-            )
-
             Text(
-                modifier = modifier
-                    .height(14.dp),
-                text = "Вход по E-mail",
-                color = Gray
+                text = "Войти с Яндекс",
+                fontSize = 17.sp,
             )
-
-            Spacer(
-                modifier.height(4.dp)
-            )
-
-            var buttonstate by remember { mutableStateOf("") }
-
-            OutlinedTextField(
-                value = buttonstate,
-                onValueChange = {buttonstate = it},
-                placeholder = { Text("example@mail.ru")},
-                modifier = Modifier
-                    .width(335.dp)
-                    .height(55.dp),
-                shape = RoundedCornerShape(12.dp),
-                enabled = true
-            )
-
-            Spacer(
-                modifier.height(24.dp)
-            )
-
-            Button(
-                onClick = { },
-                enabled = buttonstate.isNotEmpty(),
-                modifier = Modifier
-                    .width(340.dp)
-                    .height(54.dp),
-                shape = RoundedCornerShape(12.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = ButActive,
-                    contentColor = Color.White,
-                    disabledContainerColor = ButInactive,
-                    disabledContentColor = Color.White
-                )
-            ) {
-                Text(
-                    text = "Далее",
-                    fontSize = 16.sp,
-                )
-            }
-        }
-
-        Column(
-            modifier = modifier
-                .width(335.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(
-                modifier.height(660.dp)
-            )
-
-            Text(
-                text = "Или войдите с помощью",
-                fontSize = 15.sp,
-                color = Gray
-            )
-
-            Spacer(
-                modifier.height(16.dp)
-            )
-
-            OutlinedButton(
-                onClick = { },
-                modifier = Modifier
-                    .width(340.dp)
-                    .height(60.dp),
-                shape = RoundedCornerShape(12.dp),
-                colors = ButtonDefaults.outlinedButtonColors(
-                    containerColor = Color.White,
-                    contentColor = ButActive
-                ),
-                border = BorderStroke(1.5.dp, ButActive)
-            ) {
-                Text(
-                    text = "Войти с Яндекс",
-                    fontSize = 15.sp,
-                )
-            }
         }
     }
 }
